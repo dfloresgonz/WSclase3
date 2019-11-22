2081c2169b9643382ca887f4a0600c121e1d324f
diego <dfloresgonz@gmail.com>
2019-11-22 12:02:40 -0500

    subiendo mi codigo

---
+++ WcfService1/Boleta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfService1
+{
+    [DataContract]
+    public class Boleta
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public double subtotal { get; set; }
+        [DataMember]
+        public double igv { get; set; }
+        [DataMember]
+        public double total { get; set; }
+
+        public double CalcularTotal()
+        {
+            this.total = this.subtotal + this.igv;
+            return this.total;
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public Boleta GetData(double subtotal)
+        {
+            const double IGV_PORC = 0.18;
+            Boleta OBoleta = new Boleta();
+            OBoleta.id = 100;
+            OBoleta.subtotal = subtotal;
+            OBoleta.igv = subtotal * IGV_PORC;
+            OBoleta.CalcularTotal();
+            //
+            Boleta OBoleta2 = new Boleta();
+            OBoleta2.id = 100;
+            OBoleta2.subtotal = subtotal * 2;
+            OBoleta2.igv = subtotal * IGV_PORC;
+            OBoleta2.CalcularTotal();
+            List<Boleta> listBoletas = new List<Boleta>();
+            listBoletas.Add(OBoleta);
+            listBoletas.Add(OBoleta2);
+            Console.WriteLine(listBoletas);
+            //
+            return OBoleta;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
